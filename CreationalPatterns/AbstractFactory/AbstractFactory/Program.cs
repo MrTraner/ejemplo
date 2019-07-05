bd082f0b9bb794f522975d7b9c51953e813aecb3
Mauricio Ochoa <juanmauricio.ochoa@hotmail.com>
2019-07-04 21:16:24 -0500

    Add files via upload

---
+++ CreationalPatterns/AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContinentFactory africa = new AfricaFactory();
+            AnimalWorld world = new AnimalWorld(africa);
+            world.RunFoodChain();
+
+            // Create and run the American animal world
+
+            ContinentFactory america = new AmericaFactory();
+            world = new AnimalWorld(america);
+            world.RunFoodChain();
+
+            // Wait for user input
+
+            Console.ReadKey();
+        }
+    }
+ 
+}
